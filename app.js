322864b39f84d0eba63185bd11ac723714a15990
ntppmm <poomcrub.new@gmail.com>
2022-01-20 17:57:40 +0700

    a

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const path = require('path');
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,'/public/')))
 
+app.set('views','./src/views'); // ตอน render ให้มา folder นี้
+app.set('view engine','ejs');
+
 app.get("/",(req,res)=>{
 
-    res.send('Hello World 123');
+    res.render('index', {username: 'Nitipoom', customer: ['A','B','C']}); // render จาก app.set views ไฟล์ชื่อ index
 
 })
 
